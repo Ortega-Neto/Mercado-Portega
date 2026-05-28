3a1d97b11038043525e3a5f2b1a552e3af57ed46
Luiz Carlos Ortega Neto <netodm10@gmail.com>
2026-05-28 17:45:42 -0400

    feat: update icon

--- app/src/main/java/br/com/lconeto/mercadoportega/home/presentation/HomeFragment.kt
+++ app/src/main/java/br/com/lconeto/mercadoportega/home/presentation/HomeFragment.kt
@@ -35,7 +35,7 @@ class HomeFragment : Fragment() {
         binding.homeMakeList.imageContactTitle.setImageResource(R.drawable.ic_shopping_list)
 
         binding.homeShopping.textContactTitle.text = getString(R.string.home_shopping)
-        binding.homeShopping.imageContactTitle.setImageResource(R.drawable.ic_shoppin_car)
+        binding.homeShopping.imageContactTitle.setImageResource(R.drawable.ic_shopping_car)
     }
 
     private fun setupListeners() {
